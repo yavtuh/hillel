fee426c0e86af18c6dfd13ff94d9867c74ffd507
ugoldrovabriketu <103173469+ugoldrovabriketu@users.noreply.github.com>
2022-05-31 14:57:46 +0300

    hw1

--- homework1(color)/index.php
+++ homework1(color)/index.php
@@ -1,107 +1,110 @@
 <?php
+class Color
+{    /* Класс должен содержать три приватных свойства*/
+    private int $red;
+    private int $green;
+    private int $blue;
 
-
-
-class color   // Object
-{
-    // Класс должен содержать три приватных свойства:
-
-    private $red;
-    private $green;
-    private $blue;
-
-    // Конструктор класса должен принимать параметры для каждого из цветов.
-    // Constructer
-    function __construct($red, $green, $blue)
-    {
-
-        $this->set_value_red($red);
-        $this->set_value_green($green);
-        $this->set_value_blue($blue);
-    }
-
-    // Для всех свойств создать публичные геттеры и приватные сеттеры.
-
-    // В сеттерах цветов проверить передаваемое значение на диапазон от 0 до 255. Если передаваемое число выходит за диапазон - выбросить исключение.
-    // setters
-    private function set_value_red($red)
+    /* В сеттерах цветов проверить передаваемое значение на диапазон от 0 до 255. Если передаваемое число выходит за диапазон - выбросить исключение.*/
+    private function set_red($red)
     {
         if (!is_numeric($red)) {
-            throw new InvalidArgumentException("NOT A NUMBER");
+           echo "Пустое значение red";
+        }else{
+            if ( $red < 0 || $red > 255) {
+                echo "Значение ".$red." red не входит в диапазон от 0 до 255";
+            }else{
+            $this->red = $red;   
+            }
         }
-        if ($red > 255 or $red < 0) {
-            throw new InvalidArgumentException("Error Processing Request. Value must be between 0 and 255");
-        }
-        $this->red = $red;   // dont need else{} because throw is kill
     }
-
-    private function set_value_green($green)
+    private function set_green($green)
     {
         if (!is_numeric($green)) {
-            throw new InvalidArgumentException("NOT A NUMBER");
-        }
-        if ($green > 255 or $green < 0) {
-            throw new InvalidArgumentException("Error Processing Request. Value must be between 0 and 255");
-        }
-        $this->green = $green;
+            echo "Пустое значение green";
+         }else{
+             if ( $green < 0 || $green > 255) {
+                 echo "Значение ".$green." green не входит в диапазон от 0 до 255";
+             }else{
+             $this->green = $green;   
+             }
+         }
     }
-
-    private function set_value_blue($blue)
+    private function set_blue($blue)
     {
         if (!is_numeric($blue)) {
-            throw new InvalidArgumentException("NOT A NUMBER");
-        }
-        if ($blue > 255 or $blue < 0) {
-            throw new InvalidArgumentException("Error Processing Request. Value must be between 0 and 255");
-        }
-        $this->blue = $blue;
+            echo "Пустое значение blue";
+         }else{
+             if ( $blue < 0 || $blue > 255) {
+                 echo "Значение ".$blue." green не входит в диапазон от 0 до 255";
+             }else{
+             $this->blue = $blue;   
+             }
+         }
     }
-
-    // getters
-    public function get_red()
+    /*геттеры */
+    public function getRed():int
     {
         return $this->red;
     }
-
-    public function get_green()
+    public function getGreen():int
     {
         return $this->green;
     }
-
-    public function get_blue()
+    public function getBlue():int
     {
         return $this->blue;
     }
-
-    // Реализовать метод equals, который будет сравнивать объекты цветов и возвращать true или false.
-    // da fuck am I suppose to do? like this?
-    // Equals
-    public function equals($color1, $color2)
+    /*Конструктор класса должен принимать параметры для каждого из цветов*/
+    public function __construct(int $red, int $green, int $blue)
     {
-        if ($color1->get_red() == $color2->get_red() and $color1->get_green() == $color2->get_green() and $color1->get_blue() == $color2->get_blue()) {
+        $this->set_red($red);
+        $this->set_green($green);
+        $this->set_blue($blue);
+    }
+    /* Реализовать метод equals, который будет сравнивать объекты цветов и возвращать true или false.*/
+    public function equals (Color $value) :bool {
+        if (($this->red == $value) && ($this->green == $value->getGreen()) && ($this->blue == $value->getBlue())) {
             return true;
-        } else {
+        } 
+        else{
             return false;
         }
     }
-
-    // Реализовать статический метод random, который будет возвращать объект RGB цвета с случайными значениями свойств red, green, blue.
-    // ran_col method
-    public static function ran_col($some_color)
+/* Реализовать метод mix, который будет цвета. Смешивать (вычислять среднее число для каждого свойства цвета).*/
+    public function mix(Color $value): Color
     {
-        $some_color->set_value_red(rand(0, 255));
-        $some_color->set_value_green(rand(0, 255));
-        $some_color->set_value_blue(rand(0, 255));
+        $mixred = $value->getRed();
+        $mixgreen = $value->getGreen();
+        $mixblue = $value->getBlue();
+
+        $mixred = intdiv($mixred + $this->getRed(),2);
+        $mixgreen = intdiv($mixgreen + $this->getGreen(),2);
+        $mixblue = intdiv($mixblue + $this->getBlue(),2);
+
+        return (new Color ($mixred, $mixgreen, $mixblue));
+    }
+    /* Реализовать статический метод random, который будет возвращать объект RGB цвета с случайными значениями свойств red, green, blue.*/
+    public static function rand(Color $value): Color
+    {
+        $randred = $value->getRed();
+        $randgreen = $value->getGreen();
+        $randblue = $value->getBlue();
+        return (new Color ($randred, $randgreen, $randblue));
     }
 
-    // Реализовать метод mix, который будет цвета. Смешивать (вычислять среднее число для каждого свойства цвета).
-    // MIX ME DADY method
-    public function mix_me_dady($color1, $color2)
-    {
-        $new_color[] = round(($color1->get_red() + $color2->get_red()) / 2);
-        $new_color[] = round(($color1->get_green() + $color2->get_green()) / 2);
-        $new_color[] = round(($color1->get_blue() + $color2->get_blue()) / 2);
-
-        return ($new_color);
-    }
 }
+$color = new Color(200, 200, 200);
+$mixedColor = $color->mix(new Color(100, 100, 100));
+$mixedColor->getRed(); // 150
+$mixedColor->getGreen(); // 150
+$mixedColor->getBlue(); // 150
+
+echo "Реализовать метод equals, который будет сравнивать объекты цветов и возвращать true или false:<br>";
+var_dump($color->equals(new Color(100, 100, 100)));
+echo "<br>";
+echo "Реализовать метод mix, который будет цвета. Смешивать (вычислять среднее число для каждого свойства цвета):<br>";
+var_dump($mixedColor);
+echo "<br>";
+echo "Реализовать статический метод random, который будет возвращать объект RGB цвета с случайными значениями свойств red, green, blue:<br>";
+var_dump($color->rand(new Color(rand(0, 255), rand(0, 255), rand(0, 255))));
