ef7fd195c3987367ddf3b679e52e96de05f693a9
ugoldrovabriketu <103173469+ugoldrovabriketu@users.noreply.github.com>
2022-06-03 18:05:50 +0300

    hm(call)

--- homework3(__call )/index.php
+++ homework3(__call )/index.php
@@ -1,3 +1,24 @@
+<?php
 class User{
-    
+    private $name = 'Name';
+    private $age = "Age";
+    private $email = "Email";
+    public function __call($name, $arguments)
+    {
+        echo "Вызов метода '$name' "
+        . implode(', ', $arguments). "\n";
+    }
+    private function setName(){
+        return $this->name;
+    }
+    private function setAge(){
+        return $this->age;
+    }
+    public function getAll(){
+       echo $this->setAge().'<br>';
+       echo $this->setName().'<br>';
+       echo $this->setEmail().'<br>';
+    }
 }
+$user = new User;
+$user->getAll();
