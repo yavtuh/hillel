c56acafb03843c2750c705026e2f0850ae94c3d3
ugoldrovabriketu <103173469+ugoldrovabriketu@users.noreply.github.com>
2022-06-12 15:21:17 +0300

    exception fix

--- homework5/index.php
+++ homework5/index.php
@@ -1,23 +1,38 @@
 <?php
 class User{
-    
+    private int $id;
+    private int $password;
+    private int $email;
 
-    
-    public function __construct( $id,  $password, $email)
+    private function set_id($id)
     {
-        $this->id = $id;
-        $this->password = $password;
-        $this->email=$email;
-    }
-    
-    public function getUserData(){
-        
         if (!is_numeric($this->id)) {
             throw new Exception('id содержит не число');
         }
+        else{
+            $this->id = $id;
+        }
+    }
+    private function set_password($password)
+    {
         if (strlen($this->password) > 8){
             throw new Exception('password содержит более 8 символов');
-        }   
+        }  else{
+        $this->password = $password;
+        }
+    }
+    private function set_email($email)
+    {
+        $this->email=$email;
+    }
+    public function __construct( $id,  $password, $email)
+    {
+        $this->set_id($id);
+        $this->set_password($password);
+        $this->set_email($email);
+    }
+    
+    public function getUserData(){
         
         return new User($this->id, $this->password, $this->email );  
 
